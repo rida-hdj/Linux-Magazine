1658ab3cdaef2b312767f5b5b4785834e21769a1
Nothing <173809041+nothingfr87@users.noreply.github.com>
2026-06-09 14:18:49 +0300

    Added Local Storage to the website to save the current theme

--- js/script.js
+++ js/script.js
@@ -10,11 +10,25 @@ menu.addEventListener("click", () => {
 // Light/Dark Mode Switcher
 const button = document.querySelector(".switch-mode");
 
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme === "light") {
+  body.classList.add("light");
+  button.innerHTML = 'الوضع المظلم <i class="fa-solid fa-moon"></i>';
+} else {
+  body.classList.remove("light");
+  button.innerHTML = 'الوضع الفاتح <i class="fa-solid fa-sun"></i>';
+}
 button.addEventListener("click", () => {
   body.classList.toggle("light");
-  if (body.classList.contains("light")) {
-    button.innerHTML = 'الوضع المظلم  <i class="fa-solid fa-moon"></i>';
+
+  const isLight = body.classList.contains("light");
+
+  if (isLight) {
+    localStorage.setItem("theme", "light");
+    button.innerHTML = 'الوضع المظلم <i class="fa-solid fa-moon"></i>';
   } else {
-    button.innerHTML = 'الوضع الفاتح  <i class="fa-solid fa-sun"></i>';
+    localStorage.setItem("theme", "dark");
+    button.innerHTML = 'الوضع الفاتح <i class="fa-solid fa-sun"></i>';
   }
 });
